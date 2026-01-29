d8c6566c91bc60914d06c5ecafcaa3f3e0352eeb
sejin <potatobanana050129@gmail.com>
2026-01-29 09:37:42 +0900

    First commit

---
+++ src/test/java/lemon/qrordersystem/QrordersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lemon.qrordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrordersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
