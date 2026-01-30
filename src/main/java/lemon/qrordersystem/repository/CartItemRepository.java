2f7a71a6a8ebda9445663b07b27c224f133e7f55
sejin <potatobanana050129@gmail.com>
2026-01-30 09:50:04 +0900

    feat: 장바구니 아이템 레포지토리 구현

---
+++ src/main/java/lemon/qrordersystem/repository/CartItemRepository.java
@@ -0,0 +1,15 @@
+package lemon.qrordersystem.repository;
+
+import lemon.qrordersystem.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCartId(Long cartId);
+    Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
+    void deleteByCartId(Long cartId);
+}
